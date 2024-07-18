845826ca40077582e2aa2a8b5bc64f20c138d68b
benjieqiang <benjieqiang@163.com>
2024-07-18 17:58:19 +0800

    feat: add winner

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -13,6 +13,7 @@ function Square({value, onSquareClick}) {
 }
 
 export default function Board() {
+
     //Lifting state into a parent Board component,
     // creates an array with nine elements and sets each of them to null. The useState() call around it declares a squares state variable that’s initially set to that array
     const [squares, setSquares] = useState(Array(9).fill(null));
@@ -20,9 +21,42 @@ export default function Board() {
     // 设置标志位，默认是true，如果上一把
     const [xIsNext, setXIsNext] = useState(true);
 
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2], // 水平顶部行
+            [3, 4, 5], // 水平中部行
+            [6, 7, 8], // 水平底部行
+            [0, 3, 6], // 垂直左侧列
+            [1, 4, 7], // 垂直中间列
+            [2, 5, 8], // 垂直右侧列
+            [0, 4, 8], // 对角线（左上到右下）
+            [2, 4, 6]  // 对角线（右上到左下）
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            //对于每组线条，提取出对应的三个方块索引 a, b, c。
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                // 如果找到一条获胜线，返回获胜方的标记（'X' 或 'O'）
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+
     function handleClick(i) {
-        // not first click square;
-        if (squares[i]) {
+        // not first click square or winner
+        if (squares[i] || calculateWinner(squares)) {
             return;
         }
 
@@ -34,13 +68,20 @@ export default function Board() {
 
 
     return (
-        <div className={styles.grid}>
-            {/*add the value prop to each Square component rendered by the Board component*/}
-            {squares.map((value, index) => (
-                // eslint-disable-next-line react/jsx-key
-                <Square value={squares[index]} onSquareClick={() => handleClick(index)}/>
-            ))}
-        </div>
-    );
+        <>
+            <div>{status}</div>
+            <div className={styles.grid}>
+                {/*add the value prop to each Square component rendered by the Board component*/}
+                {squares.map((value, index) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <Square value={squares[index]} onSquareClick={() => handleClick(index)}/>
+                ))}
+            </div>
+        </>
+
+    )
+        ;
+
+
 };
 
