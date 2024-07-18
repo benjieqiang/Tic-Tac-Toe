ce99aeafd952c1990c989d38c5ecce8b44db6fe4
benjieqiang <benjieqiang@163.com>
2024-07-18 17:46:25 +0800

    feat: add another user

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -17,10 +17,19 @@ export default function Board() {
     // creates an array with nine elements and sets each of them to null. The useState() call around it declares a squares state variable that’s initially set to that array
     const [squares, setSquares] = useState(Array(9).fill(null));
 
+    // 设置标志位，默认是true，如果上一把
+    const [xIsNext, setXIsNext] = useState(true);
+
     function handleClick(i) {
+        // not first click square;
+        if (squares[i]) {
+            return;
+        }
+
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+        nextSquares[i] = xIsNext ? 'X' : 'O'; // 根据 xIsNext 的值确定下一个方块的符号
         setSquares(nextSquares); // creates a copy of the squares array (nextSquares) with the JavaScript slice() Array method. Then, handleClick updates the nextSquares array to add X to the first ([0] index) square.
+        setXIsNext(!xIsNext); // 切换 xIsNext 的值，确保下一步是另一个玩家，使用 !xIsNext 可以确保 xIsNext 总是取反，无论当前状态是 true 还是 false。这样做可以保证代码在逻辑上更为一致和可预测
     }
 
 
