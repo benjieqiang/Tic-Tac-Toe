f3dca1121bc06914ed946573a50d3ebf0fba4a6c
benjieqiang <benjieqiang@163.com>
2024-07-18 17:28:36 +0800

    feat: Lifting state into Board component

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -4,36 +4,33 @@ import styles from './board.module.css';
 import {useState} from 'react'; // call from your component to let it “remember” things.
 
 // @ts-ignore
-function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        // console.log('Button clicked');
-        setValue('X');
-    };
-
+function Square({value, onSquareClick}) {
     return (
-        <button className={styles.cell} onClick={handleClick}>
+        <button className={styles.cell} onClick={onSquareClick}>
             {value}
         </button>
     );
 }
 
 export default function Board() {
+    //Lifting state into a parent Board component,
+    // creates an array with nine elements and sets each of them to null. The useState() call around it declares a squares state variable that’s initially set to that array
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(i) {
+        const nextSquares = squares.slice();
+        nextSquares[i] = "X";
+        setSquares(nextSquares); // creates a copy of the squares array (nextSquares) with the JavaScript slice() Array method. Then, handleClick updates the nextSquares array to add X to the first ([0] index) square.
+    }
+
+
     return (
         <div className={styles.grid}>
             {/*add the value prop to each Square component rendered by the Board component*/}
-            <Square/>
-            <Square/>
-            <Square/>
-
-            <Square/>
-            <Square/>
-            <Square/>
-
-            <Square/>
-            <Square/>
-            <Square/>
+            {squares.map((value, index) => (
+                // eslint-disable-next-line react/jsx-key
+                <Square value={squares[index]} onSquareClick={() => handleClick(index)}/>
+            ))}
         </div>
     );
 };
